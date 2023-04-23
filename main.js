c34de0f4fb06fd524b7c2b2c46f7d4d32ab35ea8
Ajgere572 <131599005+Ajgere572@users.noreply.github.com>
2023-04-23 20:58:26 +0700

    Latifah

---
+++ main.js
@@ -0,0 +1,31 @@
+const log = document.querySelector('.log')
+const lah = document.querySelector('.lah')
+
+
+function long () {
+  alert ('masih dalam proses 🤩')
+    
+}
+function loh () {
+  log.style.display = 'block'
+  lah.style.display = 'none'
+}
+
+function jae () {
+  log.style.display = 'none'
+  lah.style.display = 'block'
+}
+
+const tou = document.querySelector ('.tou img')
+const jaja = document.getElementById ('jaja')
+
+function lohj () {
+      tou.style.display = 'none'
+      jaja.style.display = 'block'
+
+}
+function jeb () {
+      tou.style.display = 'block'
+      jaja.style.display = 'none'
+
+}
